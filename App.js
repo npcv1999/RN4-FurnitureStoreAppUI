1e2d7b00bcde9b6ce34f9fe53bf7e47f1f9d753a
Vinh Nguyen <npcv4129@gmail.com>
2021-10-09 02:21:19 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return <Text>Hello world!!!</Text>;
+};
+
+export default App;
